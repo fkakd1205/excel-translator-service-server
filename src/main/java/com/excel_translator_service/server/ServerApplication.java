97c8c08c114127b998ac5a2f02792851ebd040c3
fkakd1205 <fkakd0102@naver.com>
2022-01-04 15:19:24 +0900

    TEST : First Commit

---
+++ src/main/java/com/excel_translator_service/server/ServerApplication.java
@@ -0,0 +1,13 @@
+package com.excel_translator_service.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/excel_translator_service/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.excel_translator_service.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
